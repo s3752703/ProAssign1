1c29d9cac8f665b2a53747f0d1e419c7008e66fe
Duc Tran <s3752703@student.rmit.edu.au>
2021-09-08 23:30:30 +1000

    Simulation complete. Makefile and report needed

---
+++ DiningPhilosopher_Simulation.cpp
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <iostream>
+#define SIZE  5
+#define RUNTIME 1
+#define UPPERBOUNDTIME 1.0
+#define LOWERBOUNDTIME 0.1
+
+bool isRunning = true;
+pthread_mutex_t forks[SIZE];
+pthread_t thread[SIZE];
+int secondForkPosition[SIZE] = {4,0,1,2,3};
+
+//the function that will be used for this simulation
+
+void* philosopher(void*);
+void eat(int);
+void think(int);
+bool pickFirstFork(int);
+bool pickSecondFork(int);
+float RanFloat(float,float);
+void putFork(int);
+///////////////////////////////////////////////////////////////
+int main(int argc, char** argv){
+    
+    srand(time(NULL));
+    int i,a[5];
+    for(i=0;i<SIZE;i++){
+        pthread_mutex_init(&forks[i],NULL);
+    }
+
+    for(i=0;i<SIZE;i++){
+        a[i]=i;
+        pthread_create(&thread[i],NULL,philosopher,(void*)&a[i]);
+    }
+    sleep(RUNTIME);
+    isRunning = false;
+    for(i=0;i<SIZE;i++){
+        pthread_join(thread[i],NULL);
+    }
+    for(i=0;i<SIZE;i++){
+        pthread_mutex_destroy(&forks[i]);
+    }
+    printf("The program exit after %d seconds\n",RUNTIME);
+    return EXIT_SUCCESS;
+}
+///////////////////////////////////////////////////////////////
+float RanFloat(float a, float b) {
+    float random = ((float) rand()) / (float) RAND_MAX;
+    float diff = b - a;
+    float r = random * diff;
+    return a + r;
+}
+///////////////////////////////////////////////////////////////
+void eat(int phnum){
+    printf("Philosopher %d is eating\n",phnum);
+}
+///////////////////////////////////////////////////////////////
+void think(int phnum){
+    printf("Philosopher %d is thinking\n",phnum);
+}
+///////////////////////////////////////////////////////////////
+bool pickFirstFork(int phnum){
+    bool success = false;
+    float think_time;
+    think_time = RanFloat(UPPERBOUNDTIME,LOWERBOUNDTIME);
+    sleep(think_time);
+    success = pthread_mutex_trylock(&forks[phnum])==0;
+    if(success){
+        printf("Philosopher %d pick his own fork\n",phnum);
+        return success;
+    }
+    if(!success){
+        printf("Philosopher %d waiting for his own fork\n",phnum);
+        think(phnum);
+        think_time = RanFloat(UPPERBOUNDTIME,LOWERBOUNDTIME);
+        sleep(think_time);
+        return success;
+    }
+}
+///////////////////////////////////////////////////////////////
+bool pickSecondFork(int phnum){
+    bool success = false;
+    float think_time;
+    think_time = RanFloat(UPPERBOUNDTIME,LOWERBOUNDTIME);
+    sleep(think_time);
+    success = pthread_mutex_trylock(&forks[secondForkPosition[phnum]])==0;
+    if(success){
+        printf("Philosopher %d pick his second fork from philosopher[%d]\n",phnum,secondForkPosition[phnum]);
+        return success;
+    }
+    if(!success){
+        printf("Philosopher %d is waiting for his second fork from philosopher[%d]\n",phnum,secondForkPosition[phnum]);
+        think(phnum);
+        think_time = RanFloat(UPPERBOUNDTIME,LOWERBOUNDTIME);
+        sleep(think_time);
+        return success;
+    }
+}
+///////////////////////////////////////////////////////////////
+void putFork(int phnum){
+    pthread_mutex_unlock(&forks[secondForkPosition[phnum]]);
+    pthread_mutex_unlock(&forks[phnum]);
+    printf("Philosopher %d has finished eating\n",phnum);
+    printf("Philosopher %d left the table\n",phnum);
+}
+///////////////////////////////////////////////////////////////
+void* philosopher(void * num){
+    int phnum =*(int *) num;
+    while(isRunning){
+        bool success1,success2 = false;
+        printf("Philosopher %d enter the table\n",phnum);
+        success1 = pickFirstFork(phnum);
+        success2 = pickSecondFork(phnum);
+        if(success1 && success2){
+            eat(phnum);
+            float eat_time = RanFloat(UPPERBOUNDTIME,LOWERBOUNDTIME);
+            sleep(eat_time);
+        }
+        putFork(phnum); 
+    }
+}
---
+++ Producer_Consumer_Simulation.cpp
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <iostream>
+#define THREAD_NUM  5
+#define SIZE 10
+#define EMPTY -1
+#define RUNTIME 10
+int bucket[SIZE];
+pthread_mutex_t mutex;
+pthread_cond_t mutex_consume;
+pthread_cond_t mutex_produce;
+int producer_counter,consumer_counter = 0;
+int count=0;
+bool isRunning = true;
+
+void* producer(void* param) {
+	while(isRunning)
+	{	
+		pthread_mutex_lock(&mutex);
+		while(count>=SIZE){
+			pthread_cond_wait(&mutex_consume,&mutex);
+		}
+		// Produce
+		int x = rand()%100;
+		//Add to bucket
+		bucket[count] = x;
+		producer_counter++;
+		printf("Thread producerThread[%d] producer value %d in the bucket[%d]\n", (int)(long)param, x, count);
+		count++;
+		
+		pthread_cond_signal(&mutex_produce);
+		pthread_mutex_unlock(&mutex);
+	}
+
+}
+
+void* consumer(void* param){
+	//Consumer print out bucket
+	while(isRunning){
+		pthread_mutex_lock(&mutex);
+		while(count<=0){
+			pthread_cond_wait(&mutex_produce,&mutex);
+		}
+		int y = bucket[count-1];
+		count--;
+		bucket[count]=EMPTY;
+		printf("Thread consumerThread[%d] consumed value %d in the bucket[%d]\n", (int)(long)param, y, count);
+		consumer_counter++;
+		pthread_cond_signal(&mutex_consume);
+		pthread_mutex_unlock(&mutex);
+	}
+
+}
+
+
+
+int main(int argc, char** argv) {
+	
+	srand(time(NULL));
+	pthread_t producerthread[THREAD_NUM], consumerthread[THREAD_NUM];
+	pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&mutex_produce, NULL);
+    pthread_cond_init(&mutex_consume, NULL);
+    
+	for (int i = 0; i < SIZE; i++)
+    {
+        bucket[i] = EMPTY;
+    }
+	for (int i = 0; i < THREAD_NUM; i++) {
+		if (pthread_create(&producerthread[i], NULL, producer, (void*)(long)i) != 0) {
+			perror("Fail to create consumer thread");
+		}
+		if (pthread_create(&consumerthread[i], NULL, consumer, (void*)(long)i) != 0) {
+			perror("Fail to create consumer thread");
+		}
+	}
+	sleep(RUNTIME);
+	isRunning = false;
+
+	for (int i = 0; i < THREAD_NUM; i++) {
+		if (pthread_join(producerthread[i], NULL) != 0) {
+			perror("Failed to join thread");
+		}
+		if (pthread_join(consumerthread[i], NULL) != 0) {
+			perror("Failed to join thread");
+			}
+		}
+		
+		pthread_mutex_destroy(&mutex);
+		pthread_cond_destroy(&mutex_produce);
+		pthread_cond_destroy(&mutex_consume);
+		printf("Program exit after %d seconds",RUNTIME);
+		return EXIT_SUCCESS;
+	
+	
+}
+
