23df15a6569483cb33e932197a3673985b75d7a0
Duc Tran s3752703 <s3752703@student.rmit.edu.au>
2021-09-09 10:56:42 +1000

    Update the pick fork function to meet assingment specs

--- DiningPhilosopher_Simulation.cpp
+++ DiningPhilosopher_Simulation.cpp
@@ -8,24 +8,24 @@
 #include <sys/time.h>
 #include <iostream>
 #define SIZE  5
-#define RUNTIME 10
+#define RUNTIME 1
 #define UPPERBOUNDTIME 1.0
 #define LOWERBOUNDTIME 0.1
-
 bool isRunning = true;
 pthread_mutex_t forks[SIZE];
 pthread_t thread[SIZE];
-int secondForkPosition[SIZE] = {4,0,1,2,3};
-
+int timeEat[SIZE] = {0,0,0,0,0};
 //the function that will be used for this simulation
 
 void* philosopher(void*);
 void eat(int);
 void think(int);
-bool pickFirstFork(int);
-bool pickSecondFork(int);
+bool pickRightFork(int);
+bool pickLeftFork(int);
 float RanFloat(float,float);
 void putFork(int);
+int LEFT(int);
+int RIGHT(int);
 ///////////////////////////////////////////////////////////////
 int main(int argc, char** argv){
     
@@ -47,8 +47,22 @@ int main(int argc, char** argv){
     for(i=0;i<SIZE;i++){
         pthread_mutex_destroy(&forks[i]);
     }
+    int sum=0;
+    for(i=0;i<SIZE;i++){
+        printf("Philosopher[%d] eat %d times\n",i,timeEat[i]);
+        sum = sum+ timeEat[i];
+    }
+    printf("total time eat: %d\n",sum);
     printf("The program exit after %d seconds\n",RUNTIME);
     return EXIT_SUCCESS;
+}
+///////////////////////////////////////////////////////////////
+int LEFT(int phnum){
+    return (phnum+4)%SIZE;
+}
+///////////////////////////////////////////////////////////////
+int RIGHT(int phnum){
+    return (phnum+1)%SIZE;
 }
 ///////////////////////////////////////////////////////////////
 float RanFloat(float a, float b) {
@@ -66,18 +80,18 @@ void think(int phnum){
     printf("Philosopher %d is thinking\n",phnum);
 }
 ///////////////////////////////////////////////////////////////
-bool pickFirstFork(int phnum){
+bool pickRightFork(int phnum){
     bool success = false;
     float think_time;
     think_time = RanFloat(UPPERBOUNDTIME,LOWERBOUNDTIME);
     sleep(think_time);
-    success = pthread_mutex_trylock(&forks[phnum])==0;
+    success = pthread_mutex_trylock(&forks[RIGHT(phnum)])==0;
     if(success){
-        printf("Philosopher %d pick his own fork\n",phnum);
+        printf("Philosopher %d pick his right fork from philosopher[%d]\n",phnum,RIGHT(phnum));
         return success;
     }
 
-    printf("Philosopher %d waiting for his own fork\n",phnum);
+    printf("Philosopher %d waiting for his right fork from philosopher[%d]\n",phnum,RIGHT(phnum));
     think(phnum);
     think_time = RanFloat(UPPERBOUNDTIME,LOWERBOUNDTIME);
     sleep(think_time);
@@ -85,18 +99,18 @@ bool pickFirstFork(int phnum){
     }
 
 ///////////////////////////////////////////////////////////////
-bool pickSecondFork(int phnum){
+bool pickLeftFork(int phnum){
     bool success = false;
     float think_time;
     think_time = RanFloat(UPPERBOUNDTIME,LOWERBOUNDTIME);
     sleep(think_time);
-    success = pthread_mutex_trylock(&forks[secondForkPosition[phnum]])==0;
+    success = pthread_mutex_trylock(&forks[LEFT(phnum)])==0;
     if(success){
-        printf("Philosopher %d pick his second fork from philosopher[%d]\n",phnum,secondForkPosition[phnum]);
+        printf("Philosopher %d pick his left fork from philosopher[%d]\n",phnum,LEFT(phnum));
         return success;
     }
     
-    printf("Philosopher %d is waiting for his second fork from philosopher[%d]\n",phnum,secondForkPosition[phnum]);
+    printf("Philosopher %d is waiting for his left fork from philosopher[%d]\n",phnum,LEFT(phnum));
     think(phnum);
     think_time = RanFloat(UPPERBOUNDTIME,LOWERBOUNDTIME);
     sleep(think_time);
@@ -105,8 +119,8 @@ bool pickSecondFork(int phnum){
 }
 ///////////////////////////////////////////////////////////////
 void putFork(int phnum){
-    pthread_mutex_unlock(&forks[secondForkPosition[phnum]]);
-    pthread_mutex_unlock(&forks[phnum]);
+    pthread_mutex_unlock(&forks[LEFT(phnum)]);
+    pthread_mutex_unlock(&forks[RIGHT(phnum)]);
     printf("Philosopher %d has finished eating\n",phnum);
     printf("Philosopher %d left the table\n",phnum);
 }
@@ -114,14 +128,15 @@ void putFork(int phnum){
 void* philosopher(void * num){
     int phnum =*(int *) num;
     while(isRunning){
-        bool success1,success2 = false;
+        bool successRight,successLeft = false;
         printf("Philosopher %d enter the table\n",phnum);
-        success1 = pickFirstFork(phnum);
-        success2 = pickSecondFork(phnum);
-        if(success1 && success2){
+        successRight = pickRightFork(phnum);
+        successLeft  = pickLeftFork(phnum);
+        if(successRight && successLeft){
             eat(phnum);
             float eat_time = RanFloat(UPPERBOUNDTIME,LOWERBOUNDTIME);
             sleep(eat_time);
+            timeEat[phnum]++;
         }
         putFork(phnum); 
     }
